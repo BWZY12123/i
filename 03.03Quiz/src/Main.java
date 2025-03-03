f2ae4e145c3e04c1321328bad410f3d11bee8190
BWZY12123 <2656746694@qq.com>
2025-03-03 16:01:50 +0700

    Add files via upload

---
+++ 03.03Quiz/src/Main.java
@@ -0,0 +1,97 @@
+class LinkedList {
+    class Node {
+        int data;
+        Node next;
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    private Node head;
+
+    // Add a node at the end
+    public void add(int value) {
+        Node newNode = new Node(value);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null) {
+            temp = temp.next;
+        }
+        temp.next = newNode;
+    }
+
+    // Remove the first occurrence of a value
+    public void remove(int value) {
+        if (head == null) return;
+        if (head.data == value) {
+            head = head.next;
+            return;
+        }
+        Node temp = head;
+        while (temp.next != null && temp.next.data != value) {
+            temp = temp.next;
+        }
+        if (temp.next != null) {
+            temp.next = temp.next.next;
+        }
+    }
+
+    // Find the middle element
+    public int findMiddle() {
+        if (head == null) return -1;
+        Node slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow.data;
+    }
+
+    // Reverse the linked list in-place
+    public void reverse() {
+        Node prev = null, current = head, next = null;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+
+    // Print the linked list
+    public void printList() {
+        Node temp = head;
+        while (temp != null) {
+            System.out.print(temp.data + " -> ");
+            temp = temp.next;
+        }
+        System.out.println("null");
+    }
+
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.add(1);
+        list.add(2);
+        list.add(3);
+        list.add(4);
+        list.add(5);
+
+        System.out.println("Original List:");
+        list.printList();
+
+        list.remove(3);
+        System.out.println("After Removing 3:");
+        list.printList();
+
+        System.out.println("Middle Element: " + list.findMiddle());
+
+        list.reverse();
+        System.out.println("Reversed List:");
+        list.printList();
+    }
+}
